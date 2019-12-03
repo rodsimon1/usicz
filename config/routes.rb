1e11d9c0cc6ddb1ecd33a39cfc302090c6cefb1c
camille de bernis <camilledebernis@gmail.com>
2019-12-03 17:54:44 -0300

    cam afternoon

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get '/kitchen_sink', to: 'pages#kitchen_sink' if Rails.env.development?
+
+
 end
