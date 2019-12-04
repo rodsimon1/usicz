e237737b71a5497c6cc9ac3864d3f8a77714496a
mahan mashoof <mahan.mashoof@gmail.com>
2019-12-04 17:15:49 -0300

    just a quick file clean

--- app/services/youtube_playlist_import_service.rb
+++ app/services/youtube_playlist_import_service.rb
@@ -4,7 +4,6 @@ class YoutubePlaylistImportService
   KEY = 'AIzaSyBYH0dPlpMxGXyo-JzTOZUUVEi8hj2L574'
   TRACK_URL = "https://www.googleapis.com/youtube/v3/videos"
 
-  # https://www.youtube.com/watch?v=xnKhsTXoKCI&list=PLenUrOlreSp6EXV4PJWLEvLIdnacjn-2w
   def initialize(playlist_url)
     @playlist_id = playlist_url.gsub(/.*(&list=)/, "")
     @platform = Platform.find_by_name('Youtube')
@@ -23,19 +22,6 @@ class YoutubePlaylistImportService
       video_id = song_item['contentDetails']['videoId']
       track_query = "?part=topicDetails&id=#{video_id}&key="
       track_response = HTTParty.get(TRACK_URL + track_query + KEY)
-=begin
-      p track_response
-      p 'track_response'
-      p track_response['items']
-      p "track_response['items']"
-      p track_response['items'][0]
-      p "track_response['items'][0]"
-      p track_response['items'][0]['topicDetails']
-      p "track_response['items'][0]['topicDetails']"
-      p track_response['items'][0]['topicDetails']['topicCategories']
-      p "track_response['items'][0]['topicDetails']['topicCategories']"
-=end
-
       if track_response['items'][0] != nil
         if track_response['items'][0]['topicDetails'] != nil
           if track_response['items'][0]['topicDetails']['topicCategories'][0] != nil
@@ -44,7 +30,7 @@ class YoutubePlaylistImportService
             genres = genre_links.map { |genre_link| genre_link.gsub(/.*(wiki\/)/, "") }
 
             @genre = genres.reject{ |i| i=="Music"}.join(", ")
-        end
+          end
           songs << Song.new(title: title, genre: @genre, external_id: video_id, platform: @platform)
         end
       end
