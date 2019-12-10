d884f3253ac77d27e6d416a2baa2ef6c33e95a7d
camille de bernis <camilledebernis@gmail.com>
2019-12-10 15:28:12 -0300

    front fixes from cam

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -15,7 +15,7 @@ loaderHandledInit();
 
 initSweetalert('#MusicIcon', {
   title: "You library is empty",
-  text: "You don’t have any songs yet. To play songs, import them first",
+  text: "You don’t have any songs yet.\n To play songs, import them first :)",
   icon: "info",
   buttons: {
     import: {
